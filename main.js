c378f89d0be229f576b14ee0989d99fdb57fa0e4
matheusgarcia3346 <matheusgarcia3346@gmail.com>
2025-05-22 14:46:52 +0000

    Criação da função showItemsList para renderizar os itens do array na tela.

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ function addItem() {
 
    console.log(items)
    }
+
+   function showItemsList() {
+    const section = document.querySelector(".list")
+    
+    items.map(item, index) => {
+        sectionList.inner.html += `
+                <div class="item">
+          <div>
+            <input type="checkbox" name="list" id="item-${index}">
+            <div class="custom-checkbox">
+              <img src="./assets/assets/checked.svg" alt="checked">
+            </div>
+            <label for="item-${index}">${item.name}">Maçã</label>
+          </div>
+          <button><img src="./assets/assets/trash-icon.svg" alt="trash-icon"></button>
+        </div>
+        `
+    }
+   }
+
+
+
+   
