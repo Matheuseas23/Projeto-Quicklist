243e45be506f0fd6c82c50dcbe784898c166cf93
matheusgarcia3346 <matheusgarcia3346@gmail.com>
2025-05-26 11:15:35 +0000

    Adição e controle de tempo para que suma a divWarning depois de 4 segundos

--- main.js
+++ main.js
@@ -35,11 +35,15 @@ function addItem() {
    }
 
 function removeItems(itemName) {
-  const itemIndex = items.findIndex((item)=> item.name === itemName)
+  const itemIndex = items.findIndex((item) => item.name === itemName)
   const divWarning = document.querySelector(".warning")
 
   divWarning.classList.remove("hide-warning")
-  
+
+  setTimeout(() => {
+    divWarning.classList.add("hide-warning")
+  }, 4000)
+
   if (itemIndex !== -1) {
     items.splice(itemIndex, 1)
   }
