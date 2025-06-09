15925be5b5ad7a78c87be82af76bce0b535de5ef
matheusgarcia3346 <matheusgarcia3346@gmail.com>
2025-06-09 00:13:22 +0000

    Organização dos itens do array utilizando a função sort

--- main.js
+++ main.js
@@ -1,54 +1,86 @@
-const items = []
+
+let items = []
+
 function addItem() {
     const itemName = document.querySelector("#item").value
 
+    if (itemName === "") {
+        alert("Digíte um item válido!")
+        return
+    }
+
    const item = {
-      name: itemName,
-      checked: false
+    name: itemName,
+    checked: false
    }
 
-   
+   items.push(item)
 
-  showItemsList()
-   }
+   document.querySelector("#item").value = ""
 
-   function showItemsList() {
+   showItemsList()
+}
+
+function showItemsList() {
     const sectionList = document.querySelector(".list")
-    
+    sectionList.textContent = ""
+   
+    items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+
     items.map((item, index) => {
         sectionList.innerHTML += `
-                <div class="item">
-          <div>
-            <input type="checkbox" name="list" id="item-${index}">
-            <div class="custom-checkbox">
-              <img src="./assets/assets/checked.svg" alt="checked">
-            </div>
-            <label for="item-${index}">${item.name}</label>
-          </div>
-          <button onclick="removeItems('${item.name}')"><img src="./assets/assets/trash-icon.svg" alt="trash-icon"></button>
+        <div class="item">
+            <div>
+              <input type="checkbox" name="List" id="item-${index}" ${item.checked && "checked"}>
+              <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                <img src="./assets/assets/checked.svg" alt="checked">
+              </div>
+              <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
         </div>
+        <button onclick="removeItem('${item.name}')">
+            <img src="./assets/assets/trash-icon.svg" alt="trash-icon">
+        </button>
+    </div>
         `
     })
-   }
 
-function removeItems(itemName) {
-  const itemIndex = items.findIndex((item) => item.name === itemName)
-  const divWarning = document.querySelector(".warning")
+    localStorage.setItem("items", JSON.stringify(items))
+} 
 
-  divWarning.classList.remove("hide-warning")
-
-  setTimeout(() => {
-    divWarning.classList.add("hide-warning")
-  }, 4000)
-
-  if (itemIndex !== -1) {
-    items.splice(itemIndex, 1)
-  }
-  showItemsList()
+function checkItem(itemName) {
+   const item = items.find((item) => item.name === itemName)
+   item.checked = !item.checked
+   showItemsList()
 }
 
-function  addHideWarningClass () {
-  document.querySelector(".warning").classList.add
-}
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
 
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
    
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
+}
+
+function addHideWarningClass() {
+    document.querySelector(".warning").classList.add("hide-warning")
+}
+
+function verifyLocalStorageItems() {
+    const LocalStorageItems = localStorage.getItem("items")
+
+    if (LocalStorageItems) {
+        items = JSON.parse(LocalStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems()
